4f6633dfac8ab273c86ea162e0ddb50a96861d74
Vaidas <shibalba1@gmail.com>
2020-02-06 23:14:32 +0100

    Updates

---
+++ precache-manifest.bf82d8ea6db2b56181e6d7053e904714.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c9edd285b2626b47fd6d2eb11453a669",
+    "url": "/kapsalon/index.html"
+  },
+  {
+    "revision": "0922e506a482938baf0b",
+    "url": "/kapsalon/static/css/2.e2d51d62.chunk.css"
+  },
+  {
+    "revision": "3b5fa4005b96c0eed8dd",
+    "url": "/kapsalon/static/css/main.d498bfb3.chunk.css"
+  },
+  {
+    "revision": "0922e506a482938baf0b",
+    "url": "/kapsalon/static/js/2.135c9ec4.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/kapsalon/static/js/2.135c9ec4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3b5fa4005b96c0eed8dd",
+    "url": "/kapsalon/static/js/main.bf6625a2.chunk.js"
+  },
+  {
+    "revision": "28437d141ee7476723c8",
+    "url": "/kapsalon/static/js/runtime-main.9f7bdace.js"
+  },
+  {
+    "revision": "1201df119b56491ed128d95c25ba3dff",
+    "url": "/kapsalon/static/media/kapsalon.1201df11.jpeg"
+  }
+]);
